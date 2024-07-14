4fdb65569416af8a54b7fd7cd0961675c7e1f42d
Sevastian Caballero <sevastiancaballero321@gmail.com>
2024-07-13 23:25:24 -0500

    add categories options

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,35 +1,45 @@
 import { Link } from "react-router-dom";
-import { Input } from "antd";
-// import type { SearchProps } from "antd/es/input/Search";
-// import { useState } from "react";
+import { useState } from "react";
 
 export default function Navbar() {
   //   const [searchValue, setSearchValue] = useState<string>("");
+  const [showCategories, setShowCategories] = useState<boolean>(false);
 
-  //   const onSearch: SearchProps["onSearch"] = (value, _e, info) =>
-  //     console.log(value);
+  const toggleCategories = () => {
+    setShowCategories(!showCategories);
+  };
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    console.log(e.target.value);
+  const closeCategories = () => {
+    setShowCategories(false);
   };
 
   return (
-    <div className="flex bg-gray-500 p-4 w-full justify-between">
-      <div>
+    <nav className="flex bg-gray-500 p-4 w-full gap-6 relative">
+      <div className="pl-8">
         <Link to="/">
           <p>Logo</p>
         </Link>
       </div>
-      <div>
-        <Input.Search
-          placeholder="input search text"
-          allowClear
-          //   onSearch={onSearch}
-          onChange={handleChange}
-          style={{ width: 200 }}
-        />
+      <div className="relative">
+        <button onMouseEnter={toggleCategories}>Categories</button>
+        {showCategories && ( // Conditional rendering
+          <ul
+            className="absolute top-8 w-24 bg-white p-2 rounded-lg shadow-md z-10 transition-all"
+            onMouseLeave={closeCategories} // Cierra el menú cuando el cursor sale del menú
+          >
+            <li className="hover:bg-gray-300 p-1 transition-all rounded-md">
+              Option 1
+            </li>
+            <li className="hover:bg-gray-300 p-1 transition-all rounded-md">
+              Option 2
+            </li>
+            <li className="hover:bg-gray-300 p-1 transition-all rounded-md">
+              Option 3
+            </li>
+          </ul>
+        )}
       </div>
-      <div>
+      <div className="flex w-full justify-end">
         <ul className="flex gap-6 font-semibold">
           <li>
             <Link to="/">Home</Link>
@@ -44,6 +54,6 @@ export default function Navbar() {
           </li>
         </ul>
       </div>
-    </div>
+    </nav>
   );
 }
